0f745c814249af6ebbe4b3ad169c83333bc7b3b4
samir <shubhod2k18@gmail.com>
2023-05-22 15:53:29 +0530

    hashRouter added

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import StoreContextProvider from './context/Context'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <StoreContextProvider>
         <App />
       </StoreContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
